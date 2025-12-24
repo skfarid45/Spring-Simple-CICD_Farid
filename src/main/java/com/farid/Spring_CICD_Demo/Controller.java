835144ca7b91cd885296eaf9687ada9f1b3eb46c
Shaik Farid <shaikfarid524@gmail.com>
2025-12-24 17:01:42 +0530

    Update Controller.java

--- src/main/java/com/farid/Spring_CICD_Demo/Controller.java
+++ src/main/java/com/farid/Spring_CICD_Demo/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
- @GetMapping("/message")
+ @GetMapping("/")
     public String getMessage(){
      return "Hello , I am Shaik Fariddddd";
  }
